26c30419805877a36be847bc56dd8e0996a0b13e
sanamrahimi <sanam.rahimi@gmail.com>
2023-11-10 21:04:44 +0500

    change the app icon

---
+++ lib/app/all_screen.dart
@@ -0,0 +1,2 @@
+export 'package:flutter/material.dart';
+export 'package:whatsapp_ui/home_screen.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:whatsapp_ui/home_screen.dart';
+import 'package:whatsapp_ui/app/all_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,8 +6,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
